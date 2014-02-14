2b48faebf4d6ccb0d8360630d338ba2f947a743a
mikkokok <mikko.kokkonen@olen.to>
2014-02-14 15:24:54 +0200

    Update Pakka.java

--- Pakka.java
+++ Pakka.java
@@ -1,23 +1,37 @@
 package pokeri;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 final public class Pakka {
+	private List<Kortti> valmisPakka = new LinkedList<Kortti> ();
 	protected static enum Kortinmaa {RISTI,RUUTU,HERTTA,PATA};
-
 	
-	
+	// -- Class 
+	@SuppressWarnings("serial")
+	static private List<Kortti> staticvalmis52korttia = new LinkedList<Kortti> () {
+		{
+			for (int i=1;i<14;i++) {
+				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.HERTTA));
+				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.RISTI));
+				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.RUUTU));
+				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.PATA));
+			}
+		}
+	};
+	// --
 	private Pakka() {
 		Kortti test1 = new Kortti(1,Kortinmaa.HERTTA);
 		Kortti test2 = new Kortti(1,Kortinmaa.HERTTA);
 		if ( test1.samaMaa(test2) ) {
 			
 		}
-		System.out.println(""+test1.toString());
+		
 	}
 
-	final private class Kortti {
+	final static private class Kortti {
 			private Map<Kortinmaa,String> nameMap = new HashMap<Kortinmaa,String>();
 			private Kortinmaa maa;
 			private Kortti(int luku, Kortinmaa maa) {
@@ -36,5 +50,18 @@ final public class Pakka {
 			public String toString() {
 				return ""+nameMap.get(this.maa);
 			}
+	}
+
+	public static Pakka giverandom() {
+		Pakka ret = new Pakka();
+		for (Kortti c: Pakka.staticvalmis52korttia) {
+			ret.add(c);
+		}
+		return ret;
+	}
+
+	private void add(Kortti c) {
+		valmisPakka.add(c);
+		
 	}	
 }
