48f2e290ed5a3d71f1f7eea863533b4747125ca6
mikkokok <mikko.kokkonen@olen.to>
2014-02-23 18:58:58 +0200

    Update Pakka.java

--- Pakka.java
+++ Pakka.java
@@ -2,66 +2,79 @@ package pokeri;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 final public class Pakka {
-	private List<Kortti> valmisPakka = new LinkedList<Kortti> ();
+	private LinkedList<Kortti> valmisPakka = new LinkedList<Kortti> ();
 	protected static enum Kortinmaa {RISTI,RUUTU,HERTTA,PATA};
-	
-	// -- Class 
+
+	// Staattinen 52 kortin pakka
 	@SuppressWarnings("serial")
-	static private List<Kortti> staticvalmis52korttia = new LinkedList<Kortti> () {
+	static private LinkedList<Kortti> staticpakka = new LinkedList<Kortti> () {
 		{
 			for (int i=1;i<14;i++) {
-				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.HERTTA));
-				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.RISTI));
-				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.RUUTU));
-				staticvalmis52korttia.add(new Kortti(i,Kortinmaa.PATA));
+				staticpakka.add(new Kortti(i,Kortinmaa.HERTTA));
+				staticpakka.add(new Kortti(i,Kortinmaa.RISTI));
+				staticpakka.add(new Kortti(i,Kortinmaa.RUUTU));
+				staticpakka.add(new Kortti(i,Kortinmaa.PATA));
 			}
 		}
 	};
-	// --
-	private Pakka() {
-		Kortti test1 = new Kortti(1,Kortinmaa.HERTTA);
-		Kortti test2 = new Kortti(1,Kortinmaa.HERTTA);
-		if ( test1.samaMaa(test2) ) {
-			
-		}
+	
+	// --Staticpakka--
+	// ***************************************************
+	// Konstruktorit
+	// Ensimmäisellä tehdään normaali 52 kortin pakka
+	// Toisella tehdään pakka jossa on vain yhtä väriä
+	// ***************************************************
+	public Pakka() {
+	
+	}
+	public Pakka(String maa) {
 		
 	}
+	// ***************************************************
+	// Havainnointimetodit
+	// ***************************************************
+	// Metodi jolla annetaan kortti pakkaoliosta. 
+	// Alkuehto: Pakka pitää olla luotuna.
+	public Kortti getKortti() {
+		
+		
+		return null;
+	}
+	// Metodi jolla palautetaan kortti pakkaan.
+	// Alkuehto: Pakka pitää olla luotuna.
+	public Kortti giveKortti() {
+		
+		return null;
+	}
+	public void luePakka() {
+		for (int i=0;i<52;i++) {
+		System.out.println(valmisPakka.get(i));
+		}
+	}
+	// ***************************************************
+	// Muutosmetodit
+	// ***************************************************
+	// Metodi jolla pakka sekotetaan
+	// Alkuehto: Pakka pitää olla luotuna
+	public void teePakka() {
+		for (int i=0;i<52;i++) {
+		valmisPakka.add(staticpakka.get(i));	
+		}
+	}
 
+	// *************************************************** 
+	// Kortti classi jolla kortit luodaan
+	// ***************************************************
 	final static private class Kortti {
-			private Map<Kortinmaa,String> nameMap = new HashMap<Kortinmaa,String>();
-			private Kortinmaa maa;
-			private Kortti(int luku, Kortinmaa maa) {
-				this.maa = maa;
-				nameMap.put(Kortinmaa.HERTTA,"Hertta");
-				nameMap.put(Kortinmaa.RUUTU,"Ruutu");
-				nameMap.put(Kortinmaa.RISTI,"Risti");
-				nameMap.put(Kortinmaa.PATA,"Pata");
-			}
-			public Boolean samaMaa(Kortti toinen) {
-				if ( toinen.maa == this.maa) {
-					return true;
-				}
-				return false;
-			}
-			public String toString() {
-				return ""+nameMap.get(this.maa);
-			}
-	}
-
-	public static Pakka giverandom() {
-		Pakka ret = new Pakka();
-		for (Kortti c: Pakka.staticvalmis52korttia) {
-			ret.add(c);
+		private Map<Kortinmaa,String> maaMap = new HashMap<Kortinmaa,String>();
+		private Kortti(int luku, Kortinmaa maa) {
+			maaMap.put(Kortinmaa.HERTTA,"♥");
+			maaMap.put(Kortinmaa.RUUTU,"♦");
+			maaMap.put(Kortinmaa.RISTI,"♣");
+			maaMap.put(Kortinmaa.PATA,"♠");
 		}
-		return ret;
-	}
-
-	private void add(Kortti c) {
-		valmisPakka.add(c);
-		
-	}	
-}
+	} // Kortti class
+	} // Pakka class
